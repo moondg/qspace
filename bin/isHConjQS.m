2f2f151aa1baab6cb7e22570c07220b3a00a385b
Andreas W. <andreas.weichselbaum@gmail.com>
2023-11-10 12:49:00 -0500

    *

---
+++ bin/isHConjQS.m
@@ -0,0 +1,6 @@
+% Usage: [i,istr]=isHConjQS(A [,eps]);
+%
+%    check whether QSpace is hermitian within threshold eps (1E-12).
+%    A must be even-rank object as hyperindex is allowed.
+%
+% Wb,Aug17,06
