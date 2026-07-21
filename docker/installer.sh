a9be04de927066a946851913b4da91f0791aa864
SKey <88277450+moondg@users.noreply.github.com>
2026-07-21 17:55:31 +0900

    feat: Update installer.sh to support MATLAB R20XXY
    Now installer.sh will work for recent MATLAB official image regardless of version.

--- docker/installer.sh
+++ docker/installer.sh
@@ -6,18 +6,22 @@ sudo apt upgrade -y
 sudo apt install git libgmp-dev libmpfr-dev -y
 
 # specify folders
-QSPACE_DIR=$HOME/Documents/MATLAB/qspace-v4-pub
+QSPACE_DIR=$HOME/Documents/MATLAB/qspace
 echo "Default installation path is $QSPACE_DIR"
 
 # prepare before compile
 cd $(dirname $QSPACE_DIR)
-git clone https://bitbucket.org/qspace4u/qspace-v4-pub.git
+BASE_NAME="${QSPACE_DIR##*/}"
+git clone https://bitbucket.org/qspace4u/qspace-v4-pub.git $BASE_NAME
 mkdir $QSPACE_DIR/build
 
+# check MATLAB version
+R20XXY=$(ls /opt/matlab/ | grep R20)
+
 user_config() {
-  cat <<'EOF' > $QSPACE_DIR/system/matlab_setup_user.sh
-export MATLAB_ROOT=/opt/matlab/R2025a
-export MSLOTS=$(grep -c processor /proc/cpuinfo)
+MATLAB_SETUP_USER=$QSPACE_DIR/system/matlab_setup_user.sh
+echo "export MATLAB_ROOT=/opt/matlab/$R20XXY" >> $MATLAB_SETUP_USER
+echo "export MSLOTS=$(grep -c processor /proc/cpuinfo)" >> $MATLAB_SETUP_USER
 EOF
 }
 
@@ -26,24 +30,25 @@ bash_config() {
 # =============================================================================
 # =                             QSpace v4.0 setup                             =
 # =============================================================================
-# Assumed: Matlab R2025a container image based on Ubuntu
+# Assumed: Matlab R2025a(R20XXY) container image based on Ubuntu
 # 1) From qspace user-guide.pdf p.96, define alias
 MYMATLAB=$QSPACE_DIR
-EOF
-  cat <<'EOF' >> $HOME/.qspace_config
-alias mlsetup="source $MYMATLAB/system/matlab_setup.sh"
+alias mlsetup="source \$MYMATLAB/system/matlab_setup.sh"
 # 2) From qspace user-guide.pdf p.96-97, add path
-export PATH=$PATH:$MYMATLAB/Source
-export PATH=$PATH:$MYMATLAB/system
+export PATH=\$PATH:\$MYMATLAB/Source
+export PATH=\$PATH:\$MYMATLAB/system
 # `matlab` is in PATH "/usr/local/bin", however, `mex` is not.
-# Actually, both `matlab` and `mex` are in "/opt/matlab/R2025a/bin".
+# Actually, both `matlab` and `mex` are in "/opt/matlab/R20XXY/bin".
 # `matlab` in the "/usr/local/bin" is just symbolic link to actual path
 # Therefore, add actual path to PATH
-export PATH=/opt/matlab/R2025a/bin:$PATH
+export PATH=/opt/matlab/$R20XXY/bin:\$PATH
 # 3) Default path of LMA, RC_STORE, RC_SYNC is not proper in this case
-export LMA=$MYMATLAB/build
-export RC_STORE=$LMA/RCStore
-export RC_SYNC=$LMA/RCSync
+export LMA=\$MYMATLAB/build
+export RC_STORE=\$LMA/RCStore
+export RC_SYNC=\$LMA/RCSync
+# 4) Check GPU backend(USE_XXX options must be exclusive).
+nvidia-smi > /dev/null && export USE_CUDA=1
+# AMD_GPU_COMMAND > /dev/null && export USE_ROCM=1
 EOF
 }
 
